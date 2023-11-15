28171e52da00ecd8b269f48f9686c5a21abbdf95
hazem lachheb <lachhebhazem87@gmail.com>
2023-11-15 11:07:43 +0100

    votes

--- src/Entity/VinylMix.php
+++ src/Entity/VinylMix.php
@@ -27,7 +27,11 @@ class VinylMix
     private ?string $genre = null;
 
     #[ORM\Column]
-    private ?\DateTimeImmutable $createdAt = null;
+    private ?\DateTimeImmutable $createdAt;
+    public function __construct()
+    {
+        $this->createdAt = new \DateTimeImmutable();
+    }
 
     public function getId(): ?int
     {
---
+++ src/Entity/Votes.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VotesRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: VotesRepository::class)]
+class Votes
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
