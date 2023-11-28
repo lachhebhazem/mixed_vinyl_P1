292563c6044b5df34aa5c4d3b2ceb3a19b6a0e91
hazem lachheb <lachhebhazem87@gmail.com>
2023-11-29 00:24:49 +0100

    The Request Object

--- src/Controller/MixController.php
+++ src/Controller/MixController.php
@@ -6,6 +6,7 @@ namespace App\Controller;
 use App\Entity\VinylMix;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -30,11 +31,21 @@ class MixController extends AbstractController
             $mix->getTrackCount()
         ));
     }
-    #[Route('/mix/{id}', name: 'app_mix_show')]
+    #[Route('/mix/{id}/vote', name: 'app_mix_vote', methods: ['POST'])]
     public function show(VinylMix $mix): Response
     {
         return $this->render('mix/show.html.twig', [
             'mix' => $mix,
         ]);
+    }
+    public function vote(VinylMix $mix, Request $request): Response
+    {
+        $direction = $request->request->get('direction', 'up');
+        if ($direction === 'up') {
+            $mix->setVotes($mix->getVotes() + 1);
+        } else {
+            $mix->setVotes($mix->getVotes() - 1);
+        }
+        dd($mix);
     }
 }
